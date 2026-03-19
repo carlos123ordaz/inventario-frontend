a4be1c4fd7187d5c91c8e3b568919ee32308a968
carlosordaz <cordaz@corsusa.com>
2026-03-19 10:39:08 -0500

    vite config

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+console.log(import.meta.env.API_URL)
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 100000,
     withCredentials: true,
     headers: {
