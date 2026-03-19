e77fcb388e1f5eb60f88f15f132a6f4946108c8f
carlosordaz <cordaz@corsusa.com>
2026-03-19 10:28:28 -0500

    env api

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/api',
-    // baseURL: 'https://inventario-backend-production-b163.up.railway.app/api',
+    baseURL: import.meta.env.API_URL,
     timeout: 100000,
     withCredentials: true,
     headers: {
